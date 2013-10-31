a94b9f6b3cbe172bff82060eaa4ff3e8d8bed471
Michael Nemec <mnemec@ubuntu.(none)>
2013-10-31 15:02:01 +0000

    initial commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
-      Company <input type="text" name="email" id="email"/></br>
+      Company <input type="text" name="company" id="company"/></br>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
